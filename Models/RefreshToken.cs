81fbb87bca83e74fdf643e87d5b9df9bcc1a5a2f
Ardon <radekmedalion@gmail.com>
2025-09-21 19:54:34 +0200

    Added model for refresh token for migration

---
+++ Models/RefreshToken.cs
@@ -0,0 +1,14 @@
+﻿namespace CardCollector.Models
+{
+    public class RefreshToken
+    {
+        public int Id { get; set; }
+        public Guid Token { get; set; } = Guid.NewGuid();
+        public DateTime ExpiryDate { get; set; }
+        public bool IsRevoked { get; set; } = false;
+
+        // Relations to user
+        public Guid UserId { get; set; }
+        public User? User { get; set; }
+    }
+}
--- Models/User.cs
+++ Models/User.cs
@@ -4,6 +4,7 @@
     {
         public Guid UserId { get; set; } = Guid.NewGuid();
         public string? Username { get; set; }
+        public string? Email { get; set; }
         public string? Password { get; set; }
         public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
 
