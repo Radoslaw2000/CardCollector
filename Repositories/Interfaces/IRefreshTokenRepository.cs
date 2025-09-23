13d6c6b3033c72d4a5cefc88054b0f474157a358
Ardon <radekmedalion@gmail.com>
2025-09-23 17:27:15 +0200

    Added RefreshTokenRepository

---
+++ Repositories/Interfaces/IRefreshTokenRepository.cs
@@ -0,0 +1,12 @@
+﻿using CardCollector.Models;
+
+namespace CardCollector.Repositories.Interfaces
+{
+    public interface IRefreshTokenRepository
+    {
+        Task AddAsync(RefreshToken refreshToken);
+        Task<IEnumerable<RefreshToken>> GetActiveRefreshTokensByUserIdAsync(Guid userId);
+        Task RevokeRefreshTokenByIdAsync(Guid refreshTokenId);
+        Task SaveChangesAsync();
+    }
+}
---
+++ Repositories/RefreshTokenRepository.cs
@@ -0,0 +1,48 @@
+﻿using CardCollector.Data;
+using CardCollector.Models;
+using CardCollector.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace CardCollector.Repositories
+{
+    public class RefreshTokenRepository : IRefreshTokenRepository
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public RefreshTokenRepository(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task AddAsync(RefreshToken refreshToken)
+        {
+            await _dbContext.RefreshTokens.AddAsync(refreshToken);
+            await _dbContext.SaveChangesAsync();
+        }
+
+        public async Task<IEnumerable<RefreshToken>> GetActiveRefreshTokensByUserIdAsync(Guid userId)
+        {
+            return await _dbContext.RefreshTokens
+                .Where(rt => 
+                            rt.UserId == userId && 
+                            rt.IsRevoked == false && 
+                            rt.ExpiryDate > DateTime.UtcNow)
+                .ToListAsync();
+        }
+
+        public async Task RevokeRefreshTokenByIdAsync(Guid refreshTokenId)
+        {
+            var refreshToken = await _dbContext.RefreshTokens.FirstOrDefaultAsync(rt => rt.Token == refreshTokenId);
+            if (refreshToken == null)
+                throw new InvalidOperationException("RefreshToken not found.");
+
+            refreshToken.IsRevoked = true;
+            await _dbContext.SaveChangesAsync();
+        }
+
+        public async Task SaveChangesAsync()
+        {
+            await _dbContext.SaveChangesAsync();
+        }
+    }
+}
